7b135c8155a3cd67729bf94e503ead477f8a6141
Daniel Franco <danialtza@gmail.com>
2020-04-30 20:55:22 +0200

    Adapt smooth code to Numpy 1.18.1

--- smooth_tiled_predictions.py
+++ smooth_tiled_predictions.py
@@ -59,8 +59,8 @@ def _window_2D(window_size, power=2):
     if key in cached_2d_windows:
         wind = cached_2d_windows[key]
     else:
-        wind = _spline_window(window_size, power)
-        wind = np.expand_dims(np.expand_dims(wind, 3), 3)
+        wind = _spline_window(window_size, power)   
+        wind = np.expand_dims(np.expand_dims(wind, -1), -1)
         wind = wind * wind.transpose(1, 0, 2)
         if PLOT_PROGRESS:
             # For demo purpose, let's look once at the window:
