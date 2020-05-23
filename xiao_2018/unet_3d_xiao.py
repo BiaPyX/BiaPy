5dfad37e3c0a1910464af3bc7bb48db7d8d26d93
Daniel Franco <danialtza@gmail.com>
2020-05-23 17:49:09 +0200

    Remove L2 regularizer to all layers of Xiao 2018 proposed network

--- xiao_2018/unet_3d_xiao.py
+++ xiao_2018/unet_3d_xiao.py
@@ -11,27 +11,13 @@ from loss import custom_loss
 
 def U_Net_3D_Xiao(image_shape, lr=0.0001, num_classes=2):
     """Create 3D U-Net.
-
        Args:
             image_shape (array of 3 int): dimensions of the input image.
-
             activation (str, optional): Keras available activation type.
 
-            numInitChannels (int, optional): number of convolution channels to 
-            start with. In each downsampling/upsampling the number of filters 
-            are multiplied/divided by 2.
-
-            fixed_dropout (float, optional): dropout value to be fixed. If no 
-            value is provided the default behaviour will be to select a 
-            piramidal value stating from 0.1 and reaching 0.3 value.
-
-            spatial_dropout (bool, optional): use spatial dropout instead of the
-            "normal" dropout. 
-
-            weighted_loss (bool, optional): flag to control if a weighted loss 
-            is going to be used.
-
             lr (float, optional): learning rate value.
+
+            num_classes (int, optional): number of classes.
     
        Returns:
             model (Keras model): model containing the U-Net created.
@@ -42,8 +28,8 @@ def U_Net_3D_Xiao(image_shape, lr=0.0001, num_classes=2):
     #inputs = Input(image_shape)
     inputs = Input(dinamic_dim)
         
-    x = Conv3D(3, (3, 3, 3), activation=None, kernel_initializer='he_normal', 
-               padding='same', kernel_regularizer=l2(0.01))(inputs)
+    x = Conv3D(3, (3, 3, 3), activation=None,
+               kernel_initializer='he_normal', padding='same')(inputs)
     x = BatchNormalization()(x)
     x = ELU() (x)
 
@@ -59,7 +45,7 @@ def U_Net_3D_Xiao(image_shape, lr=0.0001, num_classes=2):
     # Decoder 
     x = UpSampling3D((1, 2, 2)) (x)
     x = Conv3D(64, (1, 1, 1), activation=None, kernel_initializer='he_normal', 
-               padding='same', kernel_regularizer=l2(0.01))(x)
+               padding='same')(x)
     x = BatchNormalization()(x)
     x = ELU() (x)
     x = Add()([s3, x])
@@ -68,7 +54,6 @@ def U_Net_3D_Xiao(image_shape, lr=0.0001, num_classes=2):
     x = UpSampling3D((1, 2, 2)) (x)
         
     # Auxiliary ouput 1 
-    #a1 = UpSampling3D((1, 2, 2)) (x)    
     a1 = UpSampling3D((1, 2, 2)) (x)    
     a1 = Conv3D(num_classes, (1, 1, 1), activation=None,
                 kernel_initializer='he_normal', padding='same',
@@ -77,7 +62,7 @@ def U_Net_3D_Xiao(image_shape, lr=0.0001, num_classes=2):
 
 
     x = Conv3D(48, (1, 1, 1), activation=None, kernel_initializer='he_normal', 
-               padding='same', kernel_regularizer=l2(0.01))(x)
+               padding='same')(x)
     x = BatchNormalization()(x)
     x = ELU() (x)
     x = Add()([s2, x])
@@ -85,12 +70,11 @@ def U_Net_3D_Xiao(image_shape, lr=0.0001, num_classes=2):
     x = residual_block(x, 48)
     x = UpSampling3D((1, 2, 2)) (x)
     x = Conv3D(32, (1, 1, 1), activation=None, kernel_initializer='he_normal', 
-               padding='same', kernel_regularizer=l2(0.01))(x)
+               padding='same')(x)
     x = BatchNormalization()(x)
     x = ELU() (x)
 
     # Auxiliary ouput 2
-    #a2 = UpSampling3D((1, 2, 2)) (x)
     a2 = Conv3D(num_classes, (1, 1, 1), activation=None,
                 kernel_initializer='he_normal', padding='same',
                 kernel_regularizer=l2(0.01))(x)
@@ -106,8 +90,7 @@ def U_Net_3D_Xiao(image_shape, lr=0.0001, num_classes=2):
 
     # Adapt the output to use softmax pixel-wise 
     x = Conv3D(num_classes, (1, 1, 1), activation=None, 
-               kernel_initializer='he_normal', padding='same', 
-               kernel_regularizer=l2(0.01))(x)
+               kernel_initializer='he_normal', padding='same')(x)
     outputs = Activation('softmax')(x)
    
     model = Model(inputs=[inputs], outputs=[outputs])
@@ -123,21 +106,17 @@ def U_Net_3D_Xiao(image_shape, lr=0.0001, num_classes=2):
 def residual_block(inp_layer, channels):
 
     a = Conv3D(channels, (1, 1, 1), activation=None,
-               kernel_initializer='he_normal', padding='same',
-               kernel_regularizer=l2(0.01))(inp_layer)
+           kernel_initializer='he_normal', padding='same')(inp_layer)
     a = BatchNormalization()(a)
     a = ELU() (a)
 
     b = Conv3D(channels, (3, 3, 3), activation=None,
-               kernel_initializer='he_normal', padding='same',
-               kernel_regularizer=l2(0.01))(inp_layer)
+               kernel_initializer='he_normal', padding='same')(inp_layer)
     b = BatchNormalization()(b)
     b = ELU() (b)
     b = Conv3D(channels, (3, 3, 3), activation=None,
-               kernel_initializer='he_normal', padding='same',
-               kernel_regularizer=l2(0.01))(b)
+               kernel_initializer='he_normal', padding='same')(b)
     b = BatchNormalization()(b)
     b = ELU() (b)
 
     return Add()([a, b])
-
