855a978cea298c51090f2a8bd45f0bd48ffcab81
Daniel Franco <danialtza@gmail.com>
2020-06-13 20:46:44 +0200

    FCN8 implementation for Oztel paper

--- oztel_2017/networks.py
+++ oztel_2017/networks.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import sys
 from tensorflow.keras import Input, Model
 from tensorflow.keras.layers import Lambda, Conv2D, Conv2DTranspose, \
-                                    MaxPooling2D, concatenate, Add
+                                    MaxPooling2D, Add, Cropping2D
 from metrics import jaccard_index_softmax, jaccard_index
 from BilinearUpSampling import *
 from tensorflow.keras.applications import VGG16
@@ -62,10 +62,11 @@ def FCN_VGG16(image_shape, activation='relu', lr=0.1,
     x = Dropout(0.5)(x)
     x = Conv2D(4096, (1, 1), activation='relu', padding='same', name='fc2')(x)
     x = Dropout(0.5)(x)
-    x = Conv2D(1, (1, 1), kernel_initializer='he_normal', activation='sigmoid',
+    x = Conv2D(2, (1, 1), kernel_initializer='he_normal', activation='sigmoid',
                padding='valid', strides=(1, 1))(x)
 
-    outputs = BilinearUpSampling2D(target_size=image_shape)(x)
+    outputs = tf.keras.layers.UpSampling2D(size=(32, 32), interpolation='bilinear')(x)
+    #outputs = BilinearUpSampling2D(target_size=image_shape)(x)
 
     model_fcn = Model(inputs=[inputs], outputs=[outputs])
 
@@ -80,6 +81,94 @@ def FCN_VGG16(image_shape, activation='relu', lr=0.1,
 #        if 'conv' in name:
 #            model_fcn.get_layer(name).set_weights(
 #                model_vgg.get_layer(name).get_weights())
+
+    # Select the optimizer
+    if optimizer == "sgd":
+        opt = tf.keras.optimizers.SGD(lr=lr, momentum=0.99, decay=0.0,
+                                      nesterov=False)
+    elif optimizer == "adam":
+        opt = tf.keras.optimizers.Adam(lr=lr, beta_1=0.9, beta_2=0.999,
+                                       epsilon=None, decay=0.0, amsgrad=False)
+    else:
+        raise ValueError("Error: optimizer value must be 'sgd' or 'adam'")
+
+    # Compile the model
+    model_fcn.compile(optimizer=opt, loss='binary_crossentropy',
+                      metrics=[jaccard_index])
+
+    return model_fcn
+
+
+
+def FCN8_VGG16(image_shape, activation='relu', lr=0.1,
+               weight_decay=0.01, optimizer="adam"):
+
+    dinamic_dim = (None,)*(len(image_shape)-1) + (1,)
+    inputs = Input(dinamic_dim, name="input")
+    #inputs = Input((image_shape[0], image_shape[1], image_shape[2]))
+
+    # Block 1
+    x = Conv2D(64, (3, 3), activation='relu', padding='same',
+               name='block1_conv1')(inputs)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same',
+               name='block1_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+
+    # Block 2
+    x = Conv2D(128, (3, 3), activation='relu', padding='same',
+               name='block2_conv1')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same',
+               name='block2_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+    # Block 3
+    x = Conv2D(256, (3, 3), activation='relu', padding='same',
+               name='block3_conv1')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same',
+               name='block3_conv2')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same',
+               name='block3_conv3')(x)
+    p3 = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+    # Block 4
+    x = Conv2D(512, (3, 3), activation='relu', padding='same',
+               name='block4_conv1')(p3)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same',
+               name='block4_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same',
+               name='block4_conv3')(x)
+    p4 = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+    # Block 5
+    x = Conv2D(512, (3, 3), activation='relu', padding='same',
+               name='block5_conv1')(p4)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same',
+               name='block5_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same',
+               name='block5_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+
+    # Last convolutional block
+    x = Conv2D(4096, (3, 3), activation='relu', padding='same', name='fc1')(x)
+    x = Dropout(0.5)(x)
+    x = Conv2D(4096, (1, 1), activation='relu', padding='same', name='fc2')(x)
+    x = Dropout(0.5)(x)
+    x = Conv2D(2, (1, 1), kernel_initializer='he_normal')(x)
+
+    u1 = Conv2DTranspose(2, kernel_size=(4, 4), strides=(2, 2), use_bias=False) (x)
+    u1 = Cropping2D(cropping=((0, 2), (0, 2))) (u1)
+    u_p4 = Conv2D(2, (1, 1), kernel_initializer='he_normal')(p4)
+    o1 = Add() ([u1, u_p4])
+
+    u2 = Conv2DTranspose(2, kernel_size=(4, 4), strides=(2, 2), use_bias=False) (o1)
+    u2 = Cropping2D(cropping=((0, 2), (0, 2))) (u2)
+    u_p3 = Conv2D(2, (1, 1), kernel_initializer='he_normal')(p3)
+    o2 = Add() ([u2, u_p3])
+
+    outputs = Conv2DTranspose(2, kernel_size=(16, 16), strides=(8, 8),
+                              use_bias=False, padding='same') (o2)
+
+    model_fcn = Model(inputs=[inputs], outputs=[outputs])
 
     # Select the optimizer
     if optimizer == "sgd":
