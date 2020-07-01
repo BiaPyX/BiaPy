e3892d6275ca6ae32f01cf6f405bc2d1b5b61f14
Daniel Franco <danialtza@gmail.com>
2020-07-01 14:01:47 +0200

    Change kernel_initializer to default

--- oztel_2017/cnn_oztel.py
+++ oztel_2017/cnn_oztel.py
@@ -1,13 +1,11 @@
-from keras.models import Model
-from keras.layers import Input, Dense, Cropping2D, Add, Activation, Flatten
-from keras.layers.core import Dropout, Lambda, SpatialDropout2D
-from keras.layers.convolutional import Conv2D, Conv2DTranspose
-from keras.layers.pooling import MaxPooling2D
-from keras.layers.merge import concatenate
-import keras 
+import tensorflow as tf
+from tensorflow.keras import Model, Input
+from tensorflow.keras.layers import Dense, Activation, Flatten, MaxPooling2D, \
+                                    Conv2D
 from metrics import jaccard_index, jaccard_index_softmax
 
-def cnn_oztel_2017(image_shape, activation='relu', lr=0.1, optimizer="sgd"):
+
+def cnn_oztel(image_shape, activation='relu', lr=0.1, optimizer="sgd"):
     """Create the CNN proposed by Oztel et. al.
 
        Args:
@@ -24,24 +22,18 @@ def cnn_oztel_2017(image_shape, activation='relu', lr=0.1, optimizer="sgd"):
             model (Keras model): model containing the CNN created.
     """
 
-    #dinamic_dim = (None,)*(len(image_shape)-1) + (1,)
-    #inputs = Input(dinamic_dim, name="input")
     inputs = Input(image_shape)
         
-    conv1 = Conv2D(32, (5, 5), activation=activation,
-                   kernel_initializer='he_normal', padding='same', 
+    conv1 = Conv2D(32, (5, 5), activation=activation, padding='same', 
                    name="conv1") (inputs)
-    p1 = MaxPooling2D((2, 2)) (conv1)
-    conv2 = Conv2D(32, (5, 5), activation=activation,
-                   kernel_initializer='he_normal', padding='same',
+    p1 = MaxPooling2D((3, 3), strides=2) (conv1)
+    conv2 = Conv2D(32, (5, 5), activation=activation, padding='same',
                    name="conv2") (p1)
-    p2 = MaxPooling2D((2, 2)) (conv2)
-    conv3 = Conv2D(64, (5, 5), activation=activation,
-                   kernel_initializer='he_normal', padding='same',
+    p2 = MaxPooling2D((3, 3), strides=2) (conv2)
+    conv3 = Conv2D(64, (5, 5), activation=activation, padding='same',
                    name="conv3") (p2)
-    p3 = MaxPooling2D((2, 2)) (conv3)
-    conv4 = Conv2D(64, (4, 4), activation=activation,
-                   kernel_initializer='he_normal', padding='same',
+    p3 = MaxPooling2D((3, 3), strides=2) (conv3)
+    conv4 = Conv2D(64, (4, 4), activation=activation, padding='same',
                    name="conv4") (p3)
 
     o = Flatten() (conv4)
@@ -52,26 +44,41 @@ def cnn_oztel_2017(image_shape, activation='relu', lr=0.1, optimizer="sgd"):
     
     # Select the optimizer
     if optimizer == "sgd":
-        opt = keras.optimizers.SGD(lr=lr, momentum=0.99, decay=0.0, 
+        opt = tf.keras.optimizers.SGD(lr=lr, momentum=0.99, decay=0.0, 
                                    nesterov=False)
     elif optimizer == "adam":
-        opt = keras.optimizers.Adam(lr=lr, beta_1=0.9, beta_2=0.999, 
+        opt = tf.keras.optimizers.Adam(lr=lr, beta_1=0.9, beta_2=0.999, 
                                     epsilon=None, decay=0.0, amsgrad=False)
     else:
         raise ValueError("Error: optimizer value must be 'sgd' or 'adam'")
         
     # Compile the model
-    model.compile(optimizer=opt, loss='binary_crossentropy',
-                  metrics=["accuracy"])
+    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=["accuracy"])
 
     return model
 
-def cnn_oztel_2017_test(model, image_shape, activation='relu', lr=0.1, 
+
+def cnn_oztel_test(model, image_shape, activation='relu', lr=0.1, 
                         optimizer="sgd"):
+    """Create the CNN proposed by Oztel et. al for testing, where the full image
+       will be fed to the network.
+
+       Args:
+            image_shape (3D tuple): dimensions of the input image.
+
+            activation (str, optional): Keras available activation type.
+
+            lr (float, optional): learning rate value.
+
+            optimizer (str, optional): optimizer used to minimize the loss
+            function. Posible options: 'sgd' or 'adam'.
+
+       Returns:
+            model (Keras model): model containing the CNN created.
+    """
 
     dinamic_dim = (None,)*(len(image_shape)-1) + (1,)
     inputs = Input(dinamic_dim, name="input")
-    #inputs = Input((image_shape[0], image_shape[1], image_shape[2]))
 
     conv1 = Conv2D(32, (5, 5), activation=activation,
                    kernel_initializer='he_normal', padding='same',
@@ -89,24 +96,9 @@ def cnn_oztel_2017_test(model, image_shape, activation='relu', lr=0.1,
                    kernel_initializer='he_normal', padding='same',
                    name="conv4") (p3)
 
-    o = Conv2D(2, (1 ,1), activation=activation, kernel_initializer='he_normal',
-               padding='valid')(conv4)
-    o = Conv2DTranspose(2, kernel_size=(4,4), strides=(2,2),
-                        kernel_initializer='he_normal')(o)
-    o = Cropping2D(((1, 1), (1, 1)))(o)
-
-    o2 = Conv2D(2, (1, 1), activation=activation)(p2)
-    o = Add()([o , o2])
-
-    o = Conv2DTranspose(2, kernel_size=(4,4), strides=(2,2), padding='valid')(o)
-    o = Cropping2D(((1, 1), (1, 1)))(o)
-
-    o1 = Conv2D(2, (1, 1), activation=activation)(p1)
-    o = Add()([o , o1])
-
-    o = Conv2DTranspose(2, kernel_size=(2,2), strides=(2,2), padding='same')(o)
-
-    outputs = Activation('softmax')(o)
+    x = Conv2D(2, (1 ,1), activation=activation, padding='same')(conv4)
+    x = Activation('softmax')(x)
+    outputs = tf.keras.layers.UpSampling2D(size=(8, 8), interpolation='bilinear')(x)
 
     model_test = Model(inputs=[inputs], outputs=[outputs])
 
@@ -118,10 +110,10 @@ def cnn_oztel_2017_test(model, image_shape, activation='relu', lr=0.1,
 
     # Select the optimizer
     if optimizer == "sgd":
-        opt = keras.optimizers.SGD(lr=lr, momentum=0.99, decay=0.0,
+        opt = tf.keras.optimizers.SGD(lr=lr, momentum=0.99, decay=0.0,
                                    nesterov=False)
     elif optimizer == "adam":
-        opt = keras.optimizers.Adam(lr=lr, beta_1=0.9, beta_2=0.999,
+        opt = tf.keras.optimizers.Adam(lr=lr, beta_1=0.9, beta_2=0.999,
                                     epsilon=None, decay=0.0, amsgrad=False)
     else:
         raise ValueError("Error: optimizer value must be 'sgd' or 'adam'")
