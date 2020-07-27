b0f2c1a089997dfcb0d8bbd6e3aae470402c8298
Daniel Franco <danialtza@gmail.com>
2020-07-27 18:50:50 +0200

    Refactor 2D UNet code

--- networks/unet.py
+++ networks/unet.py
@@ -1,31 +1,41 @@
 import tensorflow as tf
+from tensorflow.keras.layers import Dropout, SpatialDropout2D, Conv2D,\
+                                    Conv2DTranspose, MaxPooling2D, concatenate,\
+                                    ELU, BatchNormalization, Activation, \
+                                    ZeroPadding2D
 from tensorflow.keras import Model, Input
-from tensorflow.keras.layers import Dropout, Lambda, SpatialDropout2D, Conv2D, \
-                                    Conv2DTranspose, MaxPooling2D, concatenate
 from metrics import binary_crossentropy_weighted, jaccard_index, \
                     weighted_bce_dice_loss
 
-def U_Net(image_shape, activation='elu', numInitChannels=16, fixed_dropout=0.0, 
-          spatial_dropout=False, loss_type="bce", optimizer="sgd", lr=0.001,
-          fine_tunning=False):
 
-    """Create the U-Net.
+def U_Net_2D(image_shape, activation='elu', feature_maps=[32, 64, 128, 256, 512], 
+             depth=4, drop_values=[0.1,0.1,0.2,0.2,0.3], spatial_dropout=False, 
+             batch_norm=False, k_init='he_normal', loss_type="bce", 
+             optimizer="sgd", lr=0.001, fine_tunning=False):
+             
+    """Create the U-Net2D.
 
        Args:
-            image_shape (3D tuple): dimensions of the input image.
+            image_shape (2D tuple): dimensions of the input image.
 
             activation (str, optional): Keras available activation type.
 
-            numInitChannels (int, optional): number of convolution channels to
-            start with. In each downsampling/upsampling the number of filters
-            are multiplied/divided by 2.
+            feature_maps (array of ints, optional): feature maps to use on each 
+            level. Must have the same length as the depth+1.
+        
+            depth (int, optional): depth of the network.
 
-            fixed_dropout (float, optional): dropout value to be fixed. If no
+            drop_value (float, optional): dropout value to be fixed. If no
             value is provided the default behaviour will be to select a
             piramidal value stating from 0.1 and reaching 0.3 value.
 
             spatial_dropout (bool, optional): use spatial dropout instead of the
             "normal" dropout.
+
+            batch_norm (bool, optional): flag to make batch normalization.
+    
+            k_init (string, optional): kernel initialization for convolutional 
+            layers.
 
             loss_type (str, optional): loss type to use, three type available: 
             "bce" (Binary Cross Entropy) , "w_bce" (Weighted BCE, based on
@@ -35,137 +45,113 @@ def U_Net(image_shape, activation='elu', numInitChannels=16, fixed_dropout=0.0,
             function. Posible options: 'sgd' or 'adam'.
 
             lr (float, optional): learning rate value.
-        
-            fine_tunning (bool, optional): flag to freeze the encoder part for 
+
+            fine_tunning (bool, optional): flag to freeze the encoder part for
             fine tuning.
 
        Returns:
             model (Keras model): model containing the U-Net created.
     """
-    dinamic_dim = (None,)*(len(image_shape)-1) + (1,)
-    inputs = Input(dinamic_dim)
-    #inputs = Input((image_shape[0], image_shape[1], image_shape[2]))
+
+    if len(feature_maps) != depth+1:
+        raise ValueError("feature_maps dimension must be equal depth+1")
+    if len(drop_values) != depth+1:
+        raise ValueError("'drop_values' dimension must be equal depth+1")
+
+    #dinamic_dim = (None,)*(len(image_shape)-1) + (1,)
+    #inputs = Input(dinamic_dim)
+    x = Input(image_shape)
+    inputs = x
         
     if loss_type == "w_bce":
-        weights = Input((image_shape[0], image_shape[1], image_shape[2]))
+        weights = Input(image_shape)
 
-    c1 = Conv2D(numInitChannels, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (inputs)
-    if fixed_dropout == 0.0:
-        c1 = SpatialDropout2D(0.1) (c1) if spatial_dropout == True else Dropout(0.1) (c1)
-    else:
-        c1 = SpatialDropout2D(fixed_dropout) (c1) if spatial_dropout == True else Dropout(fixed_dropout) (c1)
+    # List used to access layers easily to make the skip connections of the U-Net
+    l=[]
 
-    c1 = Conv2D(numInitChannels, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (c1)
-    p1 = MaxPooling2D((2, 2)) (c1)
+    # ENCODER
+    for i in range(depth):
+        x = Conv2D(feature_maps[i], (3, 3), activation=None,
+                   kernel_initializer=k_init, padding='same') (x)
+        x = BatchNormalization() (x) if batch_norm else x 
+        x = Activation(activation) (x)
+        if drop_values is not None:
+            if spatial_dropout:
+                x = SpatialDropout2D(drop_values[i]) (x)
+            else:
+                x = Dropout(drop_values[i]) (x)
+        x = Conv2D(feature_maps[i], (3, 3), activation=None,
+                   kernel_initializer=k_init, padding='same') (x)
+        x = BatchNormalization() (x) if batch_norm else x 
+        x = Activation(activation) (x)
+
+        l.append(x)
     
-    c2 = Conv2D(numInitChannels*2, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (p1)
-    if fixed_dropout == 0.0:
-        c2 = SpatialDropout2D(0.1) (c2) if spatial_dropout == True else Dropout(0.1) (c2)
-    else:
-        c2 = SpatialDropout2D(fixed_dropout) (c2) if spatial_dropout == True else Dropout(fixed_dropout) (c2)
-    c2 = Conv2D(numInitChannels*2, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (c2)
-    p2 = MaxPooling2D((2, 2)) (c2)
+        x = MaxPooling2D((2, 2))(x)
+
+    # BOTTLENECK
+    x = Conv2D(feature_maps[depth], (3, 3), activation=None,
+               kernel_initializer=k_init, padding='same')(x)
+    x = BatchNormalization() (x) if batch_norm else x
+    x = Activation(activation) (x)
+    if drop_values is not None:
+            if spatial_dropout:
+                x = SpatialDropout2D(drop_values[depth]) (x)
+            else:
+                x = Dropout(drop_values[depth]) (x)
+    x = Conv2D(feature_maps[depth], (3, 3), activation=None,
+               kernel_initializer=k_init, padding='same') (x)
+    x = BatchNormalization() (x) if batch_norm else x
+    x = Activation(activation) (x)
+
+    # DECODER
+    for i in range(depth-1, -1, -1):
+        x = Conv2DTranspose(feature_maps[i], (2, 2), 
+                            strides=(2, 2), padding='same') (x)
+
+        # Adjust shape introducing zero padding to allow the concatenation
+        a = x.shape[1]
+        b = l[i].shape[1]
+        s = a - b
+        if s > 0:
+            l[i] = ZeroPadding2D(padding=((s,0), (s,0), (s,0))) (l[i])
+        elif s < 0:
+            x = ZeroPadding2D(padding=((abs(s),0), (abs(s),0), (abs(s),0))) (x)
+        x = concatenate([x, l[i]])
+
+        x = Conv2D(feature_maps[i], (3, 3), activation=None,
+                   kernel_initializer=k_init, padding='same') (x)
+        x = BatchNormalization() (x) if batch_norm else x
+        x = Activation(activation) (x)
+        if drop_values is not None:
+            if spatial_dropout:
+                x = SpatialDropout2D(drop_values[i]) (x)
+            else:
+                x = Dropout(drop_values[i]) (x)
+        x = Conv2D(feature_maps[i], (3, 3), activation=None,
+                   kernel_initializer=k_init, padding='same') (x)
+        x = BatchNormalization() (x) if batch_norm else x
+        x = Activation(activation) (x)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid') (x)
     
-    c3 = Conv2D(numInitChannels*4, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (p2)
-    if fixed_dropout == 0.0:
-        c3 = SpatialDropout2D(0.2) (c3) if spatial_dropout == True else Dropout(0.2) (c3)
-    else:    
-        c3 = SpatialDropout2D(fixed_dropout) (c3) if spatial_dropout == True else Dropout(fixed_dropout) (c3)
-    c3 = Conv2D(numInitChannels*4, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (c3)
-    p3 = MaxPooling2D((2, 2)) (c3)
-    
-    c4 = Conv2D(numInitChannels*8, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (p3)
-    if fixed_dropout == 0.0:
-        c4 = SpatialDropout2D(0.2) (c4) if spatial_dropout == True else Dropout(0.2) (c4)
-    else:
-        c4 = SpatialDropout2D(fixed_dropout) (c4) if spatial_dropout == True else Dropout(fixed_dropout) (c4)
-    c4 = Conv2D(numInitChannels*8, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (c4)
-    p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
-    
-    c5 = Conv2D(numInitChannels*16, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (p4)
-    if fixed_dropout == 0.0:
-        c5 = SpatialDropout2D(0.3) (c5) if spatial_dropout == True else Dropout(0.3) (c5)
-    else:
-        c5 = SpatialDropout2D(fixed_dropout) (c5) if spatial_dropout == True else Dropout(fixed_dropout) (c5)
-    c5 = Conv2D(numInitChannels*16, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (c5)
-    
-    u6 = Conv2DTranspose(numInitChannels*8, (2, 2), strides=(2, 2),
-                         padding='same') (c5)
-    u6 = concatenate([u6, c4])
-    c6 = Conv2D(numInitChannels*8, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (u6)
-    if fixed_dropout == 0.0:
-        c6 = SpatialDropout2D(0.2) (c6) if spatial_dropout == True else Dropout(0.2) (c6)
-    else:
-        c6 = SpatialDropout2D(fixed_dropout) (c6) if spatial_dropout == True else Dropout(fixed_dropout) (c6)
-    c6 = Conv2D(numInitChannels*8, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (c6)
-    
-    u7 = Conv2DTranspose(numInitChannels*4, (2, 2), strides=(2, 2),
-                         padding='same') (c6)
-    u7 = concatenate([u7, c3])
-    c7 = Conv2D(numInitChannels*4, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (u7)
-    if fixed_dropout == 0.0:
-        c7 = SpatialDropout2D(0.2) (c7) if spatial_dropout == True else Dropout(0.2) (c7)
-    else:
-        c7 = SpatialDropout2D(fixed_dropout) (c7) if spatial_dropout == True else Dropout(fixed_dropout) (c7)
-    c7 = Conv2D(numInitChannels*4, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (c7)
-    
-    u8 = Conv2DTranspose(numInitChannels*2, (2, 2), strides=(2, 2),
-                         padding='same') (c7)
-    u8 = concatenate([u8, c2])
-    c8 = Conv2D(numInitChannels*2, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (u8)
-    if fixed_dropout == 0.0:
-        c8 = SpatialDropout2D(0.1) (c8) if spatial_dropout == True else Dropout(0.1) (c8)
-    else:
-        c8 = SpatialDropout2D(fixed_dropout) (c8) if spatial_dropout == True else Dropout(fixed_dropout) (c8)
-    c8 = Conv2D(numInitChannels*2, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (c8)
-    
-    u9 = Conv2DTranspose(numInitChannels, (2, 2), strides=(2, 2),
-                         padding='same') (c8)
-    u9 = concatenate([u9, c1], axis=3)
-    c9 = Conv2D(numInitChannels, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (u9)
-    if fixed_dropout == 0.0:
-        c9 = SpatialDropout2D(0.1) (c9) if spatial_dropout == True else Dropout(0.1) (c9)
-    else:
-        c9 = SpatialDropout2D(fixed_dropout) (c9) if spatial_dropout == True else Dropout(fixed_dropout) (c9)
-    c9 = Conv2D(numInitChannels, (3, 3), activation=activation,
-                kernel_initializer='he_normal', padding='same') (c9)
-    
-    outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
-  
-    # Loss type 
+    # Loss type
     if loss_type == "w_bce":
-        model = Model(inputs=[inputs, weights], outputs=[outputs]) 
+        model = Model(inputs=[inputs, weights], outputs=[outputs])
     else:
         model = Model(inputs=[inputs], outputs=[outputs])
-    
+
     # Select the optimizer
     if optimizer == "sgd":
         opt = tf.keras.optimizers.SGD(
             lr=lr, momentum=0.99, decay=0.0, nesterov=False)
     elif optimizer == "adam":
         opt = tf.keras.optimizers.Adam(
-            lr=lr, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, 
+            lr=lr, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0,
             amsgrad=False)
     else:
         raise ValueError("Error: optimizer value must be 'sgd' or 'adam'")
-        
+
     # Fine tunning: freeze the enconder part
     if fine_tunning == True:
         print("Freezing the contracting path of the U-Net for fine tunning . . .")
@@ -176,16 +162,17 @@ def U_Net(image_shape, activation='elu', numInitChannels=16, fixed_dropout=0.0,
 
     # Compile the model
     if loss_type == "bce":
-        model.compile(optimizer=opt, loss='binary_crossentropy', 
+        model.compile(optimizer=opt, loss='binary_crossentropy',
                       metrics=[jaccard_index])
     elif loss_type == "w_bce":
-        model.compile(optimizer=opt, loss=binary_crossentropy_weighted(weights), 
+        model.compile(optimizer=opt, loss=binary_crossentropy_weighted(weights),
                       metrics=[jaccard_index])
     elif loss_type == "w_bce_dice":
-        model.compile(optimizer=opt, 
+        model.compile(optimizer=opt,
                       loss=weighted_bce_dice_loss(w_dice=0.66, w_bce=0.33),
                       metrics=[jaccard_index])
     else:
         raise ValueError("'loss_type' must be 'bce', 'w_bce' or 'w_bce_dice'")
 
     return model
+
