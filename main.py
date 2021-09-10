f2688226c170559087c5b8a29d11a77b567bb023
Daniel Franco <danialtza@gmail.com>
2021-09-10 15:51:40 +0200

    Instances+BCD+2D is not implemented

--- main.py
+++ main.py
@@ -114,6 +114,8 @@ if __name__ == '__main__':
         else:
             if cfg.MODEL.N_CLASSES > 1:
                 raise ValueError("Not implemented pipeline option")
+            if cfg.DATA.CHANNELS == "BCD" and cfg.PROBLEM.NDIM == '2D':
+                raise ValueError("Not implemented pipeline option")
 
     if cfg.DATA.VAL.FROM_TRAIN and cfg.DATA.VAL.SPLIT_TRAIN <= 0:
         raise ValueError("'DATA.VAL.SPLIT_TRAIN' needs to be > 0 when 'DATA.VAL.FROM_TRAIN' == True")
