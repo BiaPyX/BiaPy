4766dc4021ab3165858a4aac0f4640b2b2b44819
Daniel Franco <danialtza@gmail.com>
2021-10-26 08:33:25 +0200

    Minor bug for 2D instance when creating channeled data

--- data/__init__.py
+++ data/__init__.py
@@ -29,11 +29,12 @@ def create_instance_channels(cfg, data_type='train'):
     tag = "TRAIN" if data_type == "train" else "VAL"
     X, _, _, filenames = f_name(getattr(cfg.DATA, tag).PATH, return_filenames=True)
     print("Creating X_{} channels . . .".format(data_type))
-    if isinstance(X, list):
-        for i in tqdm(range(len(X))):
-            X[i] = X[i].transpose((0,3,1,2,4))
-    else:
-        X = X.transpose((0,3,1,2,4))
+    if cfg.PROBLEM.NDIM == '3D':
+        if isinstance(X, list):
+            for i in tqdm(range(len(X))):
+                X[i] = X[i].transpose((0,3,1,2,4))
+        else:
+            X = X.transpose((0,3,1,2,4))
     save_npy_files(X, data_dir=getattr(cfg.DATA, tag).INSTANCE_CHANNELS_DIR, filenames=filenames,
                    verbose=cfg.TEST.VERBOSE)
 
@@ -43,11 +44,13 @@ def create_instance_channels(cfg, data_type='train'):
         for i in tqdm(range(len(Y))):
             Y[i] = labels_into_bcd(Y[i], mode=cfg.DATA.CHANNELS, save_dir=getattr(cfg.PATHS, tag+'_INSTANCE_CHANNELS_CHECK'),
                                    fb_mode=cfg.DATA.CONTOUR_MODE)
-            Y[i] = Y[i].transpose((0,3,1,2,4))
+            if cfg.PROBLEM.NDIM == '3D':
+                Y[i] = Y[i].transpose((0,3,1,2,4))
     else:
         Y = labels_into_bcd(Y, mode=cfg.DATA.CHANNELS, save_dir=getattr(cfg.PATHS, tag+'_INSTANCE_CHANNELS_CHECK'),
                             fb_mode=cfg.DATA.CONTOUR_MODE)
-        Y = Y.transpose((0,3,1,2,4))
+        if cfg.PROBLEM.NDIM == '3D':
+            Y = Y.transpose((0,3,1,2,4))
     save_npy_files(Y, data_dir=getattr(cfg.DATA, tag).INSTANCE_CHANNELS_MASK_DIR, filenames=filenames,
                    verbose=cfg.TEST.VERBOSE)
     return filenames
@@ -65,11 +68,12 @@ def create_test_instance_channels(cfg):
     f_name = load_data_from_dir if cfg.PROBLEM.NDIM == '2D' else load_3d_images_from_dir
     X_test, _, _, test_filenames = f_name(cfg.DATA.TEST.PATH, return_filenames=True)
     print("Creating X_test channels . . .")
-    if isinstance(X_test, list):
-        for i in tqdm(range(len(X_test))):
-            X_test[i] = X_test[i].transpose((0,3,1,2,4))
-    else:
-        X_test = X_test.transpose((0,3,1,2,4))
+    if cfg.PROBLEM.NDIM == '3D':
+        if isinstance(X_test, list):
+            for i in tqdm(range(len(X_test))):
+                X_test[i] = X_test[i].transpose((0,3,1,2,4))
+        else:
+            X_test = X_test.transpose((0,3,1,2,4))
     save_npy_files(X_test, data_dir=cfg.DATA.TEST.INSTANCE_CHANNELS_DIR, filenames=test_filenames,
                    verbose=cfg.TEST.VERBOSE)
 
@@ -80,10 +84,12 @@ def create_test_instance_channels(cfg):
             for i in tqdm(range(len(Y_test))):
                 Y_test[i] = labels_into_bcd(Y_test[i], mode=cfg.DATA.CHANNELS, save_dir=cfg.PATHS.TEST_INSTANCE_CHANNELS_CHECK,
                                             fb_mode=cfg.DATA.CONTOUR_MODE)
-                Y_test[i] = Y_test[i].transpose((0,3,1,2,4))
+                if cfg.PROBLEM.NDIM == '3D':
+                    Y_test[i] = Y_test[i].transpose((0,3,1,2,4))
         else:
             Y_test = labels_into_bcd(Y_test, mode=cfg.DATA.CHANNELS, save_dir=cfg.PATHS.TEST_INSTANCE_CHANNELS_CHECK,
                                      fb_mode=cfg.DATA.CONTOUR_MODE)
-            Y_test = Y_test.transpose((0,3,1,2,4))
+            if cfg.PROBLEM.NDIM == '3D':
+                Y_test = Y_test.transpose((0,3,1,2,4))
         save_npy_files(Y_test, data_dir=cfg.DATA.TEST.INSTANCE_CHANNELS_MASK_DIR, filenames=test_filenames,
                        verbose=cfg.TEST.VERBOSE)
