d54704808e67485160623fa9a5880e4e4762ed55
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2022-06-02 15:01:52 +0200

    Create csv with index column.
    For consistency with the default in napari.

--- utils/scripts/detection_probs_to_points.py
+++ utils/scripts/detection_probs_to_points.py
@@ -137,7 +137,7 @@ for i in range(0, len(new_points)):
 # create data frame in Napari CSV format
 df_save = pd.DataFrame(list(zip(z_axis,x_axis,y_axis)), columns =['axis-0', 'axis-1', 'axis-2'])
 # save to file
-df_save.to_csv( out_csv, index=False )
+df_save.to_csv( out_csv, index=True )
 
 if verbose:
     print( 'Filtered points saved in ' + out_csv )
