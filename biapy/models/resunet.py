5505d3e3c0499fbed2e4f162e665a529931ca110
Daniel Franco <daniel.franco@dipc.org>
2024-02-19 17:42:45 +0100

    Correct typo in some models

--- biapy/models/resunet.py
+++ biapy/models/resunet.py
@@ -143,7 +143,7 @@ class ResUNet(nn.Module):
 
         self.apply(self._init_weights)
 
-     def forward(self, x) -> torch.Tensor | List[torch.Tensor]:
+    def forward(self, x) -> torch.Tensor | List[torch.Tensor]:
         # Super-resolution
         if self.pre_upsampling is not None:
             x = self.pre_upsampling(x)
