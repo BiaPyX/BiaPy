Fix PSNR calculation in I2I and SR workflows

biapy/engine/super_resolution.py
@@ -207,9 +207,10 @@ class Super_resolution_Workflow(Base_Workflow):
         # Calculate PSNR
         if pred.dtype == np.dtype('uint16'):
             pred = pred.astype(np.float32)
-        if self._Y.dtype == np.dtype('uint16'):
-            self._Y = self._Y.astype(np.float32)
+        
         if self.cfg.DATA.TEST.LOAD_GT or self.cfg.DATA.TEST.USE_VAL_AS_TEST:
+            if self._Y.dtype == np.dtype('uint16'):
+                self._Y = self._Y.astype(np.float32)
             psnr_merge_patches = self.metrics[0](torch.from_numpy(pred), torch.from_numpy(self._Y))
             self.stats['psnr_merge_patches'] += psnr_merge_patches
 