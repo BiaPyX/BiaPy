cbc831d50b9d6e6a8e2b95da273015692b70bd03
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2025-07-24 08:36:42 +0200

    Correct and complete docstrings in single_data_3D_generator.py

--- biapy/data/generators/single_data_3D_generator.py
+++ biapy/data/generators/single_data_3D_generator.py
@@ -1,3 +1,10 @@
+"""
+3D single image data generator for BiaPy.
+
+This module provides the Single3DImageDataGenerator class, which generates batches of
+3D images with on-the-fly augmentation for deep learning workflows. It is based on
+imgaug, microDL, and custom augmentors for flexible data pipelines.
+"""
 import numpy as np
 import random
 
@@ -7,9 +14,7 @@ from biapy.data.generators.single_base_data_generator import SingleBaseDataGener
 
 class Single3DImageDataGenerator(SingleBaseDataGenerator):
     """
-    Custom 3D data generator based on `imgaug <https://github.com/aleju/imgaug-doc>`_ and our own
-    `augmentors.py <https://github.com/BiaPyX/BiaPy/blob/master/biapy/data/generators/augmentors.py>`_
-    transformations. This generator will yield an image and its corresponding mask.
+    Custom 3D data generator based on `imgaug <https://github.com/aleju/imgaug-doc>`_ and our own `augmentors.py <https://github.com/BiaPyX/BiaPy/blob/master/biapy/data/generators/augmentors.py>`_ transformations. This generator will yield an image and its corresponding mask.
 
     Based on `microDL <https://github.com/czbiohub/microDL>`_ and
     `Shervine's blog <https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly>`_.
@@ -21,10 +26,33 @@ class Single3DImageDataGenerator(SingleBaseDataGenerator):
     """
 
     def __init__(self, zflip=False, **kwars):
+        """
+        Initialize the Single3DImageDataGenerator.
+
+        Parameters
+        ----------
+        zflip : bool, optional
+            Whether to apply flips in the z dimension.
+        **kwars : dict
+            Keyword arguments passed to the base SingleBaseDataGenerator.
+        """
         super().__init__(**kwars)
         self.zflip = zflip
 
     def apply_transform(self, image):
+        """
+        Apply transformations to the input image, including optional z-flip.
+
+        Parameters
+        ----------
+        image : 4D Numpy array
+            Input image to transform. E.g. ``(z, y, x, channels)``.
+
+        Returns
+        -------
+        image : 4D Numpy array
+            Transformed image. E.g. ``(z, y, x, channels)``.
+        """
         # Transpose them so we can merge the z and c channels easily.
         # z, y, x, c --> x, y, z, c
         image = image.transpose((2, 1, 0, 3))
