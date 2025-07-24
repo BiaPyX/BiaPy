2f6095f66a13a8cb5d6b574cd7c394b44c446fff
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2025-07-24 10:48:25 +0200

    Add docstring to config init

--- biapy/config/__init__.py
+++ biapy/config/__init__.py
@@ -1,1 +1,7 @@
+"""
+BiaPy configuration package.
+
+This package provides configuration management utilities and default settings
+for BiaPy workflows, including experiment setup, model parameters, and system options.
+"""
 from .config import *
