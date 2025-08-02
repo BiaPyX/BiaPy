8cba482b93b92158bba619b507c7a740555d201d
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2025-08-02 08:08:50 +0200

    Simpler wrapper code.

--- biapy/models/resunet_plus_plus.py
+++ biapy/models/resunet_plus_plus.py
@@ -1,31 +1,9 @@
 # This file serves as a wrapper to allow Sphinx to import resunet++.py
 #
 # Since the original file name 'resunet++.py' is not a valid Python
-# module name, we use importlib to load it programmatically.
+# module name, we load it programmatically.
 
-import importlib.util
-import sys
-import os
-from importlib import import_module
+from biapy.models.resunet_plus_plus import ResUNetPlusPlus
 
-# Define the full path to the original file
-mdl = import_module("biapy.models.resunet++")
-model_file = os.path.abspath(mdl.__file__)
-current_dir = os.path.dirname(model_file)
-original_file_path = os.path.join(current_dir, "resunet++.py")
-
-# Create a module spec for the original file
-spec = importlib.util.spec_from_file_location("resunet++", original_file_path)
-
-# Create a new module based on the spec
-resunet_module = importlib.util.module_from_spec(spec)
-
-# Add the module to the system path so other imports can find it
-sys.modules["resunet++"] = resunet_module
-
-# Execute the module to load its contents (e.g., classes, functions)
-spec.loader.exec_module(resunet_module)
-
-# Now, we can access the class from the loaded module and make it
-# available in our wrapper.
-from resunet_module import ResUNetPlusPlus
+# Re-export the class with the same name
+ResUNetPlusPlus = ResUNetPlusPlus
