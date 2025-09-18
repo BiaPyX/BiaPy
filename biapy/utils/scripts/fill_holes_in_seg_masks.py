f53806cd34338bc0db1b64cc3cf1795475d77638
Daniel Franco <danialtza@gmail.com>
2025-09-18 17:07:54 +0200

    Upgrade fill holes in segmentation masks aux script

--- biapy/utils/scripts/fill_holes_in_seg_masks.py
+++ biapy/utils/scripts/fill_holes_in_seg_masks.py
@@ -10,14 +10,14 @@ parser = argparse.ArgumentParser(description="Fill tiny holes in semantic/instan
 
 parser.add_argument("-input_label_dir", "--input_label_dir", required=True, help="Directory to the folder where the labels to fbe fixed reside")
 parser.add_argument("-output_label_dir", "--output_label_dir", required=True, help="output folder to store the fixed labels")
+parser.add_argument("-is_3d", "--is_3d", action='store_true', help="Flag to indicate if the input is 3D")
 parser.add_argument("-BiaPy_dir", "--BiaPy_dir", required=True, help="BiaPy directory")
 args = vars(parser.parse_args())
 
-# python fill_holes_in_seg_masks.py --input_label_dir /scratch/dfranco/thesis/data2/dfranco/datasets/oocyte/arturo_instances/v2/y 
-#   --output_label_dir /scratch/dfranco/thesis/data2/dfranco/datasets/oocyte/arturo_instances/v3/y --BiaPy_dir ../../
+# python fill_holes_in_seg_masks.py --input_label_dir /data/dfranco/thesis/data2/dfranco/datasets/oocyte/arturo_instances/v2/y --output_label_dir /scratch/dfranco/thesis/data2/dfranco/datasets/oocyte/arturo_instances/v3/y --BiaPy_dir ../../ --is_3d
 
 sys.path.insert(0, args['BiaPy_dir'])
-from biapy.data.data_manipulation import save_tif, imread
+from biapy.data.data_manipulation import save_tif, read_img_as_ndarray
 
 data_dir = args['input_label_dir']
 out_dir = args['output_label_dir']
@@ -28,31 +28,20 @@ ids = sorted(next(os.walk(data_dir))[2])
 for n, id_ in tqdm(enumerate(ids), total=len(ids)):
 
     # Load image
-    if id_.endswith('.npy'):
-        img = np.load(os.path.join(data_dir, id_))
-    else:
-        img, _ = imread(os.path.join(data_dir, id_))
-    img, _ = np.squeeze(img)
+    img = read_img_as_ndarray(os.path.join(data_dir, id_), is_3d=args['is_3d'])
 
-    if img.ndim < 3:
-        raise ValueError("Read image seems to be 2D: {}. Path: {}".format(img.shape, os.path.join(data_dir, id_)))
-
-    # Make sure the image is (z,y,x,c)
-    if img.ndim == 3: 
-        img = np.expand_dims(img, -1)
-    else:
-        min_val = min(img.shape)
-        channel_pos = img.shape.index(min_val)
-        if channel_pos != 3:
-            new_pos = [x for x in range(4) if x != channel_pos]+[channel_pos,]
-            img = img.transpose(new_pos)
-    
     # Fill tiny holes 
-    new_img = np.zeros(img.shape, dtype=np.uint16)
-    for c in range(img.shape[-1]):
-        for z in range(len(img)):
-            instances_in_slice = np.unique(img[z,...,c])[1:]
+    new_img = np.zeros(img.shape, dtype=img.dtype)
+    if args['is_3d']:
+        for c in range(img.shape[-1]):
+            for z in range(len(img)):
+                instances_in_slice = np.unique(img[z,...,c])[1:]
+                for l in instances_in_slice:
+                    new_img[z,...,c] += (fill_voids.fill(img[z,...,c]==l)*l).astype(img.dtype)
+    else:
+        for c in range(img.shape[-1]):
+            instances_in_slice = np.unique(img[...,c])[1:]
             for l in instances_in_slice:
-                new_img[z,...,c] += (fill_voids.fill(img[z,...,c]==l)*l).astype(np.uint16)
+                new_img[...,c] += (fill_voids.fill(img[...,c]==l)*l).astype(img.dtype)
 
     save_tif(np.expand_dims(new_img,0), out_dir, filenames=[id_], verbose=True)
