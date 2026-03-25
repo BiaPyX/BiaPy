Small fix to normalization due to recent changes

biapy/data/generators/pair_base_data_generator.py
@@ -510,7 +510,6 @@ class PairBaseDataGenerator(Dataset, metaclass=ABCMeta):
                     instance_problem=instance_problem,
                     apply_norm=False
                 )
-                print("Sample {} mask normalization info: {}".format(i, new_mask_norm))
                 if self.mask_norm is None:
                     self.mask_norm = new_mask_norm
                 else:
@@ -897,7 +896,10 @@ class PairBaseDataGenerator(Dataset, metaclass=ABCMeta):
             )
 
         if not first_load:
-            img, _ = normalize_image(img, norm_module=self.X.dataset_info[sample.fid].norm_info)
+            xnorm_info = norm_module=self.X.dataset_info[sample.fid].norm_info
+            if xnorm_info is None:
+                xnorm_info = self.norm_module
+            img, _ = normalize_image(img, norm_module=xnorm_info)
             mask, _ = normalize_mask(mask, norm_module=self.mask_norm)
             assert isinstance(img, np.ndarray) and isinstance(mask, np.ndarray)
 

biapy/data/generators/single_base_data_generator.py
@@ -396,7 +396,10 @@ class SingleBaseDataGenerator(Dataset, metaclass=ABCMeta):
 
         # X normalization
         if not first_load:
-            img, _ = normalize_image(img, norm_module=self.X.dataset_info[sample.fid].norm_info)
+            xnorm_info = norm_module=self.X.dataset_info[sample.fid].norm_info
+            if xnorm_info is None:
+                xnorm_info = self.norm_module
+            img, _ = normalize_image(img, norm_module=xnorm_info)
             assert isinstance(img, np.ndarray)
 
         if self.convert_to_rgb and img.shape[-1] == 1:

biapy/data/generators/test_pair_data_generators.py
@@ -290,8 +290,10 @@ class test_pair_data_generator(Dataset):
             if not first_load:
                 # Normalization
                 img = np.array(img)
-                norm_info = self.X.dataset_info[sample.fid].norm_info if self.X.dataset_info[sample.fid].norm_info is not None else self.norm_module
-                img, norm_extra_info = normalize_image(img, norm_module=norm_info)
+                xnorm_info = norm_module=self.X.dataset_info[sample.fid].norm_info
+                if xnorm_info is None:
+                    xnorm_info = self.norm_module
+                img, norm_extra_info = normalize_image(img, norm_module=xnorm_info)
                 assert isinstance(img, np.ndarray)
                 if self.provide_Y:
                     mask = np.array(mask)

biapy/data/generators/test_single_data_generator.py
@@ -225,8 +225,10 @@ class test_single_data_generator(Dataset):
         norm_extra_info = None
         if not first_load:
             img = np.array(img)
-            norm_info = self.X.dataset_info[sample.fid].norm_info if self.X.dataset_info[sample.fid].norm_info is not None else self.norm_module
-            img, norm_extra_info = normalize_image(img, norm_module=norm_info)
+            xnorm_info = norm_module=self.X.dataset_info[sample.fid].norm_info
+            if xnorm_info is None:
+                xnorm_info = self.norm_module
+            img, norm_extra_info = normalize_image(img, norm_module=xnorm_info)
             assert isinstance(img, np.ndarray)
 
         if self.convert_to_rgb and img.shape[-1] == 1: