9669f75cb873dfc8b8d53a7cceb7fb866428e9e0
Daniel Franco <danialtza@gmail.com>
2026-05-15 14:49:20 +0200

    Improve tif_to_h5.py aux script

--- biapy/utils/scripts/tif_to_h5.py
+++ biapy/utils/scripts/tif_to_h5.py
@@ -1,33 +1,47 @@
 import os
+import argparse
 import h5py
 import numpy as np
-from skimage.io import imread
 from tqdm import tqdm
-from skimage import measure, feature
-from scipy import ndimage
-from PIL import ImageEnhance, Image
-from os import path
+from biapy.data.data_manipulation import read_img_as_ndarray
 
-img_shape = (4096, 4096)
-input_dir = "/home/user/input"
-output_dir = "/home/user/output"
-h5_filename = '0_human_instance_seg_pred.h5'
-#h5_filename = '1_rat_instance_seg_pred.h5'
 
-input_ids = sorted(next(os.walk(input_dir))[2])
-h5file_name = os.path.join(output_dir, h5_filename)
-os.makedirs(output_dir, exist_ok=True)
+def parse_args():
+    parser = argparse.ArgumentParser(description="Stack TIFF files from a directory into a single HDF5 file.")
+    parser.add_argument("input_dir", type=str, help="Directory containing input TIFF files.")
+    parser.add_argument("output_dir", type=str, help="Directory where the HDF5 file will be saved.")
+    parser.add_argument("h5_filename", type=str, help="Name of the output HDF5 file (e.g. output.h5).")
+    parser.add_argument("--is_3d", action="store_true", help="Set if the images are 3D.")
+    parser.add_argument(
+        "--compression",
+        type=str,
+        default="lzf",
+        choices=["gzip", "lzf", "none"],
+        help="Compression algorithm for the HDF5 dataset (default: lzf).",
+    )
+    return parser.parse_args()
 
-# Allocate memory for the predictions
-pred_stack = np.zeros((len(input_ids),) + img_shape, dtype=np.int64)
 
-# Read all the images
-for n, id_ in tqdm(enumerate(input_ids), total=len(input_ids)):
-    img = imread(os.path.join(input_dir, id_))
-    pred_stack[n] = img
+def main():
+    args = parse_args()
+    compression = None if args.compression == "none" else args.compression
 
-# Create the h5 file (using lzf compression to save space)
-h5f = h5py.File(h5file_name, 'w')
-h5f.create_dataset('main', data=pred_stack, compression="lzf")
-h5f.close()
+    input_ids = sorted(next(os.walk(args.input_dir))[2])
 
+    imgs = []
+    for id_ in tqdm(input_ids, desc="Reading"):
+        img = read_img_as_ndarray(os.path.join(args.input_dir, id_), is_3d=args.is_3d)
+        imgs.append(img)
+
+    pred_stack = np.stack(imgs, axis=0)
+    print(f"Stack shape: {pred_stack.shape}")
+
+    os.makedirs(args.output_dir, exist_ok=True)
+    h5_path = os.path.join(args.output_dir, args.h5_filename)
+    with h5py.File(h5_path, "w") as h5f:
+        h5f.create_dataset("main", data=pred_stack, compression=compression)
+    print(f"Saved {h5_path}")
+
+
+if __name__ == "__main__":
+    main()
