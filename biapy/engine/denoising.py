Fix small bug in denoising metric calculation due to recent PR

biapy/engine/denoising.py
@@ -278,11 +278,11 @@ class Denoising_Workflow(Base_Workflow):
         with torch.no_grad():
             for i, metric in enumerate(list_to_use):
                 # Nafnet for Gan With Supervised
-                if _targets.shape[1] == _output.shape[1]:
+                if self.cfg.PROBLEM.DENOISING.LOAD_GT_DATA:
                     target_for_metric = _targets.contiguous()
                 # Normal N2Void
                 else:
-                    target_for_metric = metric(_output.contiguous(), _targets[:, _output.shape[1]:].contiguous())
+                    target_for_metric = _targets[:, _output.shape[1]:].contiguous()
                 val = metric(_output.contiguous(), target_for_metric)
                 val = val.item() if not torch.isnan(val) else 0
                 out_metrics[list_names_to_use[i]] = val