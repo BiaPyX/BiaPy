b34281b12523528e1bfb6dd1d3324e8a6da3d129
Daniel Franco <danialtza@gmail.com>
2026-09-09 06:27:00 +0000

    Prepare next release

--- biapy/__init__.py
+++ biapy/__init__.py
@@ -11,10 +11,10 @@ This `__init__.py` file serves as the entry point for the BiaPy package,
 exposing the `BiaPy` class and defining the `main` function for command-line
 execution.
 
-Version: 3.7.0
+Version: 3.7.1
 """
 
-__version__ = "3.7.0"
+__version__ = "3.7.1"
 
 import argparse
 import os
